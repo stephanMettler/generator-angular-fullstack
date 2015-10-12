4e5c80c07f1b56072efdaa13a574dca8fdc8c312
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-10-12 11:43:43 +0200

    Changes to regex instead of string

--- app/templates/gulpfile.babel(gulp).js
+++ app/templates/gulpfile.babel(gulp).js
@@ -319,8 +319,8 @@ gulp.task('wiredep:client', () => {
             exclude: [
                 /bootstrap-sass-official/,
                 /bootstrap.js/,
-                '/json3/',
-                '/es5-shim/',
+                /json3/,
+                /es5-shim/,
                 /bootstrap.css/,
                 /font-awesome.css/
             ],
